5efb31b1fd35e6648211faceece959a5b0362ea6
Steven Davies <swdavies@me.com>
2013-03-25 00:36:30 -0400

    v20130325.1
    new version from jen... new header format

--- _versions/share.js
+++ _versions/share.js
@@ -1,21 +1,21 @@
-/**
-*
+﻿/**
+* 
 * Phonegap share plugin for Android
 * Kevin Schaul 2011
 *
 */
 
-var Share = function() {};
-            
-Share.prototype.show = function(content, success, fail) {
-    return cordova.exec( function(args) {
+var Share = function () { };
+
+Share.prototype.show = function (content, success, fail) {
+    return cordova.exec(function (args) {
         success(args);
-    }, function(args) {
+    }, function (args) {
         fail(args);
     }, 'Share', '', [content]);
 };
 
-if(!window.plugins) {
+if (!window.plugins) {
     window.plugins = {};
 }
 if (!window.plugins.share) {
